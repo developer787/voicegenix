0698cc834f64cc754ca85ce43d7b5a303cc2bb32
Alfredo Colon <developer787@users.noreply.github.com>
2023-04-27 12:34:35 +0000

    test

--- audio_splitter.py
+++ audio_splitter.py
@@ -1,43 +1,31 @@
 import os
-import librosa
-import numpy as np
-from pyannote.core import Segment
 from pydub import AudioSegment
-from pyannote.audio.features import Pretrained
+from resemblyzer import VoiceEncoder, preprocess_wav
+from pathlib import Path
 
-def split_audio_by_speakers(audio_file, output_folder='output'):
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
 
-    # Load the pre-trained model
-    MODEL_NAME = "dia_dihard"
-    model = Pretrained(validate_dir=None, epoch=None, device=None, batch_size=None)
-    model.load(MODEL_NAME)
+def split_audio_file(input_audio_file: str):
+    wav_fpath = Path(input_audio_file)
+    wav = preprocess_wav(wav_fpath)
+    encoder = VoiceEncoder("cpu")
 
-    # Apply the pretrained model
-    hypothesis = model.predict(audio_file)
+    embedding = encoder.embed_utterance(wav)
+    speaker_embeds = [embedding]
 
-    segments = []
-    for segment, _, speaker in hypothesis.itertracks(yield_label=True):
-        segments.append((Segment(segment.start, segment.end), speaker))
+    times, speakers = encoder.find_speaker_times(wav, speaker_embeds)
 
-    audio = AudioSegment.from_wav(audio_file)
-    duration_ms = len(audio)
+    audio = AudioSegment.from_wav(input_audio_file)
 
-    for idx, (segment, speaker_label) in enumerate(segments):
-        start_time = int(segment.start * 1000)
-        end_time = int(segment.end * 1000)
-        start_time = max(start_time, 0)
-        end_time = min(end_time, duration_ms)
+    for i, (start, end) in enumerate(times):
+        speaker_path = os.path.join("speakers", "speaker_" + str(speakers[i]))
+        os.makedirs(speaker_path, exist_ok=True)
 
-        speaker_audio = audio[start_time:end_time]
+        segment = audio[start * 1000 : end * 1000] # pydub works with milliseconds
+        segment.export(
+            os.path.join(speaker_path, f"segment_{i}.wav"), format="wav"
+        )
 
-        # Create a subdirectory for each speaker
-        speaker_folder = os.path.join(output_folder, f"speaker_{speaker_label}")
-        os.makedirs(speaker_folder, exist_ok=True)
 
-        # Save current speaker's audio segment in their subdirectory
-        output_path = os.path.join(speaker_folder, f"speaker_{speaker_label}_{idx}.wav")
-        speaker_audio.export(output_path, format="wav")
-
-    return output_folder
+if __name__ == "__main__":
+    input_audio_file = "input_audio_file.wav"
+    split_audio_file(input_audio_file)
