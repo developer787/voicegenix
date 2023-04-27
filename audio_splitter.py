0df1583439ccf15eb9c8072c1b4bf2574c39d958
Alfredo Colon <developer787@users.noreply.github.com>
2023-04-27 10:39:22 +0000

    fixes

--- audio_splitter.py
+++ audio_splitter.py
@@ -3,44 +3,51 @@ import librosa
 import numpy as np
 from pyannote.core import Segment
 from pydub import AudioSegment
-from pyannote.audio.features import Precomputed
-from pyannote.audio.embedding.extraction import SequenceEmbedding
-from pyannote.audio.tasks.speaker_diarization import SpeakerDiarization
-
+from pyannote.pipeline import Pipeline
+from pyannote.database import get_protocol, FileFinder
 
 def split_audio_by_speakers(audio_file, output_folder='output'):
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
 
-    # Load the audio
-    audio, sr = librosa.load(audio_file, sr=None, mono=True)
+    # Define a custom function for speaker diarization
+    class SpeakerDiarization(Pipeline):
+        def __init__(self):
+            super().__init__()
 
-    # Preprocess
-    # Apply preprocessing steps like denoising, normalization, etc.
-
-    # Voice activity detection and embedding
-    embedding = SequenceEmbedding()
-
-    # Train or use pre-trained SpeakerDiarization pipeline
-    pipeline = SpeakerDiarization(embedding)
-    hypothesis = pipeline({"audio": audio_file})
-
-    segments = []
-    for segment, _, speaker in hypothesis.itertracks(yield_label=True):
-        segments.append((Segment(segment.start, segment.end), speaker))
+            self.embedding = get_protocol("pyannote/database/demo/embedding")
+            self.similarity = self.embedding.get_loader("similarity")
+        
+        def apply(self, current_file):
+            speaker_embedding = self.similarity(current_file)
+            return speaker_embedding
+        
+    pipeline = SpeakerDiarization()
+    scores = pipeline.apply({
+        "audio": audio_file,
+        "database": "custom",
+        "annotated": Segment(0, 60), # Change to the actual duration of your audio file
+    })
 
     audio = AudioSegment.from_wav(audio_file)
     duration_ms = len(audio)
 
-    for idx, (segment, speaker_label) in enumerate(segments):
-        start_time = int(segment.start * 1000)
-        end_time = int(segment.end * 1000)
-        start_time = max(start_time, 0)
-        end_time = min(end_time, duration_ms)
+    # Choose your own threshold for splitting audio based on speaker similarity scores
+    threshold = 0.95 
+    segments = []
+    for idx, (start_time, end_time) in enumerate(scores.labels()):
+        speaker_label = scores.Y[scores.labels()].get((start_time, end_time))[0]
+        if np.mean(speaker_label) > threshold:
+            segments.append((start_time, end_time))
 
-        speaker_audio = audio[start_time:end_time]
-        output_path = os.path.join(output_folder, f"speaker_{speaker_label}_{idx}.wav")
-        
+    for idx, (start_time, end_time) in enumerate(segments):
+        start_time_ms = int(float(start_time) * 1000)
+        end_time_ms = int(float(end_time) * 1000)
+        start_time_ms = max(start_time_ms, 0)
+        end_time_ms = min(end_time_ms, duration_ms)
+
+        speaker_audio = audio[start_time_ms:end_time_ms]
+        output_path = os.path.join(output_folder, f"speaker_{idx}.wav")
         speaker_audio.export(output_path, format="wav")
 
     return output_folder
